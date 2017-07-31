3f23409c1e06afdd9a5e032dc7da5ceebb9a5644
Shilpa Bhat <shilpa.bhat@apple.com>
2017-07-31 14:36:28 -0700

    Udemy course

---
+++ Projects/RayWenderlich/Artistry/Artistry/ArtistTableViewCell.swift
@@ -0,0 +1,16 @@
+//
+//  ArtistTableViewCell.swift
+//  Artistry
+//
+//  Created by Shilpa Bhat on 10/7/17.
+//  Copyright © 2017 RayWenderlich. All rights reserved.
+//
+
+import UIKit
+
+class ArtistTableViewCell: UITableViewCell {
+  
+  @IBOutlet weak var bioLabel: UILabel!
+  @IBOutlet weak var nameLabel: UILabel!
+  @IBOutlet weak var artistImageView: UIImageView!
+}
---
+++ Projects/RayWenderlich/Artistry/Artistry/WorkTableViewCell.swift
@@ -0,0 +1,16 @@
+//
+//  WorkTableViewCell.swift
+//  Artistry
+//
+//  Created by Shilpa Bhat on 13/7/17.
+//  Copyright © 2017 RayWenderlich. All rights reserved.
+//
+
+import UIKit
+
+class WorkTableViewCell: UITableViewCell {
+  
+  @IBOutlet weak var workImageView: UIImageView!
+  @IBOutlet weak var workTitleLabel: UILabel!
+  @IBOutlet weak var moreInfoTextView: UITextView!
+}
